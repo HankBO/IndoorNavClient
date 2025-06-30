f1bf4382e15f3fd8df6fd3d7360f8d3f1193c012
HankBO <bohaihbo@163.com>
2025-06-30 14:47:51 +0100

    Initial Commit

---
+++ IndoorNavClient/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IndoorNavClient
+//
+//  Created by Hai Bo on 2025/6/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ IndoorNavClientTests/IndoorNavClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  IndoorNavClientTests.swift
+//  IndoorNavClientTests
+//
+//  Created by Hai Bo on 2025/6/30.
+//
+
+import Testing
+@testable import IndoorNavClient
+
+struct IndoorNavClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
