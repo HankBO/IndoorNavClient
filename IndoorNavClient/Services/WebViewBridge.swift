d995eeb68283aff7f495f78c32ec72a2bdb37225
HankBO <bohaihbo@163.com>
2025-07-01 16:21:54 +0100

    init project structure

---
+++ IndoorNavClient/Services/WebViewBridge.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewBridge.swift
+//  IndoorNavClient
+//
+//  Created by Hai Bo on 2025/7/1.
+//
+
+import WebKit
+
+protocol WebViewBridgeDelegate: AnyObject {
+    
+}
+
---
+++ IndoorNavClient/Services/WiFiScanner.swift
@@ -0,0 +1,14 @@
+//
+//  WiFiScanner.swift
+//  IndoorNavClient
+//
+//  Created by Hai Bo on 2025/7/1.
+//
+
+import CoreWLAN
+
+class WiFiScanner {
+    init () {
+        
+    }
+}
