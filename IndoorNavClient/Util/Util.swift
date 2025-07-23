91b5b8b35897f0b58f4a8f58bb0447c89a2a4979
HankBO <bohaihbo@163.com>
2025-07-23 22:37:36 +0100

    add funciton of formatted timestamp

--- IndoorNavClient/Util/Util.swift
+++ IndoorNavClient/Util/Util.swift
@@ -16,3 +16,13 @@ func dictToJsonString(_ dict: [String: Any]) -> String? {
         return nil
     }
 }
+
+func getCurrentTimeByHHmmssSSS() -> String {
+    let timestamp = NSDate().timeIntervalSince1970
+    let date = Date(timeIntervalSince1970: timestamp)
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm:ss.SSS"
+    let formattedTime = formatter.string(from: date)
+    // print(formattedTime) // Output: "14:30:25.123"
+    return formattedTime
+}
